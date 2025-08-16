12eff648b9e0196ef851ea8e56dd7cee5660736e
yunlinzhang <changyunlin@hotmail.com>
2025-08-16 20:45:55 +1200

    feat:add up two linked list

---
+++ src/main/java/com/lynn/datastructure/leetcode/easy/AddTwoSum.java
@@ -0,0 +1,57 @@
+package com.lynn.datastructure.leetcode.easy;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+public class AddTwoSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return head.next;
+    }
+
+
+}
