8b8dbbc34413470e15eb2ce5649a889f91f0abb5
yunlinzhang <changyunlin@hotmail.com>
2025-08-16 12:31:46 +1200

    feat: add TwoSum to repo

---
+++ src/main/java/com/lynn/datastructure/leetcode/easy/TwoSum.java
@@ -0,0 +1,64 @@
+package com.lynn.datastructure.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * You can return the answer in any order.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ * <p>
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ * <p>
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ * <p>
+ * <p>
+ * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+}
